bab287d5909d95d0df5cf5796b729e35c8faf662
Beruta <biruktawitshiferaw522@gmail.com>
2024-10-26 16:02:34 +0300

    add about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
+        possimus mollitia labore ab excepturi nostrum. Nam distinctio explicabo
+        pariatur accusantium est, fuga enim repudiandae debitis assumenda
+        voluptate nisi, officiis laboriosam!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
+        possimus mollitia labore ab excepturi nostrum. Nam distinctio explicabo
+        pariatur accusantium est, fuga enim repudiandae debitis assumenda
+        voluptate nisi, officiis laboriosam!
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem
+        possimus mollitia labore ab excepturi nostrum. Nam distinctio explicabo
+        pariatur accusantium est, fuga enim repudiandae debitis assumenda
+        voluptate nisi, officiis laboriosam! Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Voluptatem possimus mollitia labore ab
+        excepturi nostrum. Nam distinctio explicabo pariatur accusantium est,
+        fuga enim repudiandae debitis assumenda voluptate nisi, officiis
+        laboriosam!
+      </p>
+    </div>
+  );
+}
+
+export default About;
